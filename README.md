526ec379fd8be38b92db90df39c7875f65638e51
KSherazi <kecoston@gmail.com>
2020-07-28 18:13:18 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# generate-password
+# Generate a Password
+
+- About -
+For this project we were requested to create a series of prompts that would determine the parameters for a password that would be 8- 128 characters long. We were provided with the HTML and CSS for the project and build off of the Javascript file. 
+
+- Process - 
+My first step was to create all the variables for the project. This included what strings would be pulled into the array, the prompts and confirms to gather the user's perameters, and the open array to collect the user's perameters. After the variables were set, 
---
+++ script.js
@@ -0,0 +1,67 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Ive added the split on the variable to break up the array. 
+var lowerOp = "abcdefghijklmnopqrstuvwxyz".split("")
+var upperOp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+var numOp = "1234567890".split("")
+var specialCharOp = "!@#%^&*?".split("")
+
+// This Array will be the elements selected to pull in to complete the password
+var chosenArray = []
+
+// These are the prompts that will direct the user to select the elements for the password
+var lengthPass = prompt("How many characters would you like your password to be? Length can be between 8- 128 characters");
+var lowerLetters = confirm("Would you like to use lowercase letters?");
+var upperLetters = confirm("Would you like to use uppercase letters?");
+var num = confirm("Would you like to use numbers?");
+var special = confirm("Would you like to use special characters?");
+
+// This converts the users input to numbers
+lengthPass = parseInt(lengthPass)
+
+// This function is a loop that pulls the users input and creates the chosenArray
+function addToArray(confirm, arr){
+    if (confirm) {
+        for (let i = 0; i<arr.length; i++){
+            chosenArray.push(arr[i])
+        }
+
+    }
+
+}
+
+//Calls the function for chosenArray
+addToArray(lowerLetters, lowerOp)
+addToArray(upperLetters, upperOp)
+addToArray(num, numOp)
+addToArray(special, specialCharOp)
+
+
+// This function is a loop that pulls the number of characters requested from the user and creates a random pull of characters
+// from the chosenArray to generate the password. 
+function generatePassword(){
+    var password =""
+    for (var i = 0; i<lengthPass; i++){
+        var randomIndex = Math.floor(Math.random() * chosenArray.length)
+        var randomElement = chosenArray[randomIndex]
+    password += randomElement
+    console.log(randomIndex)
+    }
+    console.log(password)
+    return password
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
